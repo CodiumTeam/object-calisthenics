bba51c0ca23fc7f9433355882f0f04b568bfc4b5
Jordi Anguela <jordi@codium.team>
2024-10-17 16:06:31 +0200

    extract Amount class

--- src/CurrencyConverter.ts
+++ src/CurrencyConverter.ts
@@ -1,21 +1,29 @@
+class Amount {
+  public amount: number;
+
+  constructor(amount: number) {
+    this.amount = amount;
+  }
+}
+
 export class CurrencyConverter {
   private readonly EUR_TO_DOLLAR = 1.1329;
   private readonly EUR_TO_JPY = 128.121;
 
   convert(amount: number, fromCurrency: string, toCurrency: string): number {
-    return this.convert_calisthenics(fromCurrency, toCurrency, amount);
+    return this.convert_calisthenics(new Amount(amount), fromCurrency, toCurrency).amount;
   }
 
-  private convert_calisthenics(fromCurrency: string, toCurrency: string, amount: number) {
+  private convert_calisthenics(amount: Amount, fromCurrency: string, toCurrency: string) : Amount {
     if (fromCurrency == "EUR" && toCurrency == "USD") {
-      return Math.round(amount * this.EUR_TO_DOLLAR * 100) / 100;
+      return new Amount(Math.round(amount.amount * this.EUR_TO_DOLLAR * 100) / 100);
     }
     if (fromCurrency == "EUR" && toCurrency == "JPY") {
-      return Math.round(amount * this.EUR_TO_JPY);
+      return new Amount(Math.round(amount.amount * this.EUR_TO_JPY));
     }
 
     if (fromCurrency == "JPY" && toCurrency == "USD") {
-      return Math.round(amount * this.EUR_TO_DOLLAR / this.EUR_TO_JPY * 100) / 100;
+      return new Amount(Math.round(amount.amount * this.EUR_TO_DOLLAR / this.EUR_TO_JPY * 100) / 100);
     }
 
     return amount;
