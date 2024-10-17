e01e1c6e9f67467fcf9b11616da5faaaf8ce0112
Luis Rovirosa Mairlot <luis@codium.team>
2024-10-17 16:38:36 +0200

    Amount.round

--- src/CurrencyConverter.ts
+++ src/CurrencyConverter.ts
@@ -1,46 +1,50 @@
 class Amount {
-  public amount: number;
+    public amount: number;
 
-  constructor(amount: number) {
-    this.amount = amount;
-  }
+    constructor(amount: number) {
+        this.amount = amount;
+    }
+
+    round(decimals: number): Amount {
+        return new Amount(Math.round(this.amount * 10 ** decimals) / 10 ** decimals);
+    }
 }
 
 enum Currency {
-  USD,
-  EUR,
-  JPY,
+    USD,
+    EUR,
+    JPY,
 }
 
 class Money {
-  constructor(public amount: Amount, public currency: Currency) {
-  }
+    constructor(public amount: Amount, public currency: Currency) {
+    }
 }
 
 export class CurrencyConverter {
-  private readonly EUR_TO_DOLLAR = 1.1329;
-  private readonly EUR_TO_JPY = 128.121;
+    private readonly EUR_TO_DOLLAR = 1.1329;
+    private readonly EUR_TO_JPY = 128.121;
 
-  private convert_calisthenics(money: Money, toCurrency: Currency) : Amount {
-    if (money.currency == Currency.EUR && toCurrency == Currency.USD) {
-      return new Amount(Math.round(new Amount(money.amount.amount * this.EUR_TO_DOLLAR).amount * 100) / 100);
+    private convert_calisthenics(money: Money, toCurrency: Currency): Amount {
+        if (money.currency == Currency.EUR && toCurrency == Currency.USD) {
+            return new Amount(money.amount.amount * this.EUR_TO_DOLLAR).round(2);
+        }
+        if (money.currency == Currency.EUR && toCurrency == Currency.JPY) {
+            return new Amount(money.amount.amount * this.EUR_TO_JPY).round(0);
+        }
+
+        if (money.currency == Currency.JPY && toCurrency == Currency.USD) {
+            return new Amount(money.amount.amount * this.EUR_TO_DOLLAR / this.EUR_TO_JPY).round(2);
+        }
+
+        return money.amount;
     }
-    if (money.currency == Currency.EUR && toCurrency == Currency.JPY) {
-      return new Amount(Math.round(new Amount(money.amount.amount * this.EUR_TO_JPY).amount));
+
+    convert(amount: number, fromCurrency: string, toCurrency: string): number {
+        return this.convert_calisthenics(
+            new Money(new Amount(amount),
+                Currency[fromCurrency as keyof typeof Currency]),
+            Currency[toCurrency as keyof typeof Currency]
+        ).amount;
     }
-
-    if (money.currency == Currency.JPY && toCurrency == Currency.USD) {
-      return new Amount(Math.round(new Amount(money.amount.amount * this.EUR_TO_DOLLAR / this.EUR_TO_JPY).amount * 100) / 100);
-    }
-
-    return money.amount;
-  }
-
-  convert(amount: number, fromCurrency: string, toCurrency: string): number {
-    return this.convert_calisthenics(
-      new Money(new Amount(amount),
-      Currency[fromCurrency as keyof typeof Currency]),
-      Currency[toCurrency as keyof typeof Currency]
-    ).amount;
-  }
 }
