5f932b6838ed002ee1c8d72d8ddcd2876f08d9cc
Hugo Chinchilla <hugo@codium.team>
2024-10-17 16:22:41 +0200

    introduce currency type

--- src/CurrencyConverter.ts
+++ src/CurrencyConverter.ts
@@ -6,26 +6,36 @@ class Amount {
   }
 }
 
+enum Currency {
+  USD,
+  EUR,
+  JPY,
+}
+
 export class CurrencyConverter {
   private readonly EUR_TO_DOLLAR = 1.1329;
   private readonly EUR_TO_JPY = 128.121;
 
-  convert(amount: number, fromCurrency: string, toCurrency: string): number {
-    return this.convert_calisthenics(new Amount(amount), fromCurrency, toCurrency).amount;
-  }
-
-  private convert_calisthenics(amount: Amount, fromCurrency: string, toCurrency: string) : Amount {
-    if (fromCurrency == "EUR" && toCurrency == "USD") {
+  private convert_calisthenics(amount: Amount, fromCurrency: Currency, toCurrency: Currency) : Amount {
+    if (fromCurrency == Currency.EUR && toCurrency == Currency.USD) {
       return new Amount(Math.round(amount.amount * this.EUR_TO_DOLLAR * 100) / 100);
     }
-    if (fromCurrency == "EUR" && toCurrency == "JPY") {
+    if (fromCurrency == Currency.EUR && toCurrency == Currency.JPY) {
       return new Amount(Math.round(amount.amount * this.EUR_TO_JPY));
     }
 
-    if (fromCurrency == "JPY" && toCurrency == "USD") {
+    if (fromCurrency == Currency.JPY && toCurrency == Currency.USD) {
       return new Amount(Math.round(amount.amount * this.EUR_TO_DOLLAR / this.EUR_TO_JPY * 100) / 100);
     }
 
     return amount;
+  }
+
+  convert(amount: number, fromCurrency: string, toCurrency: string): number {
+    return this.convert_calisthenics(
+      new Amount(amount),
+      Currency[fromCurrency as keyof typeof Currency],
+      Currency[toCurrency as keyof typeof Currency]
+    ).amount;
   }
 }
