b4aa035d768ba1ee71199a70ee4042a0a0184e70
Jordi Anguela <jordi@codium.team>
2024-10-17 15:57:12 +0200

    extract method to avoid breaking the tests

--- src/CurrencyConverter.ts
+++ src/CurrencyConverter.ts
@@ -1,17 +1,21 @@
 export class CurrencyConverter {
-  private readonly EUR_TO_DOLAR = 1.1329;
+  private readonly EUR_TO_DOLLAR = 1.1329;
   private readonly EUR_TO_JPY = 128.121;
 
   convert(amount: number, fromCurrency: string, toCurrency: string): number {
+    return this.convert_calisthenics(fromCurrency, toCurrency, amount);
+  }
+
+  private convert_calisthenics(fromCurrency: string, toCurrency: string, amount: number) {
     if (fromCurrency == "EUR" && toCurrency == "USD") {
-      return Math.round(amount * this.EUR_TO_DOLAR * 100) / 100;
+      return Math.round(amount * this.EUR_TO_DOLLAR * 100) / 100;
     }
     if (fromCurrency == "EUR" && toCurrency == "JPY") {
       return Math.round(amount * this.EUR_TO_JPY);
     }
 
     if (fromCurrency == "JPY" && toCurrency == "USD") {
-      return Math.round(amount * this.EUR_TO_DOLAR / this.EUR_TO_JPY * 100) / 100;
+      return Math.round(amount * this.EUR_TO_DOLLAR / this.EUR_TO_JPY * 100) / 100;
     }
 
     return amount;
