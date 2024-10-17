1d7ff8099cd340bfdc05a24e39f8aa2c79147ca6
Hugo Chinchilla <hugo@codium.team>
2024-10-17 15:22:49 +0200

    use triangulation to seguir...

--- src/CurrencyConverter.ts
+++ src/CurrencyConverter.ts
@@ -1,5 +1,5 @@
 export class CurrencyConverter {
   convert(amount: number, fromCurrency: string, toCurrency: string): number {
-    return 10;
+    return amount;
   }
 }
--- tests/CurrencyConverter.spec.ts
+++ tests/CurrencyConverter.spec.ts
@@ -1,9 +1,12 @@
 import { CurrencyConverter } from '../src/CurrencyConverter'
 
 describe('Currency Converter', () => {
-  it('convert Euros to Euros return the same amount', () => {
-    const currencyConverter = new CurrencyConverter()
 
-    expect(currencyConverter.convert(10, "EUR", "EUR")).toBe(10)
+  test.each([
+    [10,],
+    [20,],
+  ])('convert Euros to Euros return the same amount', (amount) => {
+    const currencyConverter = new CurrencyConverter()
+    expect(currencyConverter.convert(amount, "EUR", "EUR")).toBe(amount)
   })
 })
